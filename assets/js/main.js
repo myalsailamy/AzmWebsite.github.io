d785961e426426ecb90dae96e4aca0b040d499c5
mohammed yahia alsailamy <me_mo_ry_123@hotmail.com>
2020-07-21 15:04:05 +0300

    Job details page and remove extra js plusin

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,29 +134,6 @@
     return false;
   });
 
-  // jQuery counterUp
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      900: {
-        items: 2
-      }
-    }
-  });
 
   // Init AOS
   function aos_init() {
@@ -167,10 +144,9 @@
       mirror: false
     });
   }
+
   $(window).on('load', function() {
     aos_init();
   });
-
-
 
 })(jQuery);
